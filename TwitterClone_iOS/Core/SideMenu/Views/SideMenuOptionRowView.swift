44303c3bb9889963fa2b5180532d95e102643beb
Saurav-Suman-TM <saurav@teachmint.com>
2023-08-24 16:41:42 +0530

    separates out views for side menu

---
+++ TwitterClone_iOS/Core/SideMenu/Views/SideMenuOptionRowView.swift
@@ -0,0 +1,33 @@
+//
+//  SideMenuOptionRowView.swift
+//  TwitterClone_iOS
+//
+//  Created by SAURAV SUMAN on 17/05/23.
+//
+
+import SwiftUI
+
+struct SideMenuOptionRowView: View {
+    let viewModel: SideMenuViewModel
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: viewModel.imageName)
+                .font(.headline)
+                .foregroundColor(.gray)
+            
+            Text(viewModel.title)
+                .font(.subheadline)
+                .foregroundColor(.black)
+            
+            Spacer()
+        }
+        .frame(height: 40)
+    }
+}
+
+struct SideMenuOptionRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuOptionRowView(viewModel: .profile)
+    }
+}
--- TwitterClone_iOS/Core/SideMenu/Views/SideMenuView.swift
+++ TwitterClone_iOS/Core/SideMenu/Views/SideMenuView.swift
@@ -9,50 +9,48 @@ import SwiftUI
 
 struct SideMenuView: View {
     var body: some View {
-        
         VStack(alignment: .leading, spacing: 40)  {
-            VStack(alignment: .leading) {
-                Circle()
-                    .frame(width: 50, height: 50)
-                
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Bruce Wayne")
-                        .font(.headline)
-                        .bold()
-                        .foregroundColor(.black)
-                    
-                    Text("@batman")
-                        .foregroundColor(.gray)
-                        .font(.caption)
-                }
-                
-                UserStatsView().padding(.vertical)
-            }
-            
-            VStack() {
-                ForEach(SideMenuViewModel.allCases, id: \.rawValue) { option in
-                    HStack(spacing: 16) {
-                        Image(systemName: option.imageName)
-                            .font(.headline)
-                            .foregroundColor(.gray)
-                        
-                        Text(option.title)
-                            .font(.subheadline)
-                        
-                        Spacer()
-                    }
-                    .frame(height: 40)
-                }
-            }
-            
+            userInfoView
+            userSideMenuOptions
             Spacer()
         }
         .padding()
     }
-}
-
-struct SideMenuView_Previews: PreviewProvider {
-    static var previews: some View {
-        SideMenuView()
+    
+    var userInfoView: some View {
+        VStack(alignment: .leading) {
+            Circle().frame(width: 50, height: 50)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text("User Name")
+                    .font(.headline)
+                    .bold()
+                    .foregroundColor(.black)
+                
+                Text("@user-name")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+            
+            UserStatsView().padding(.vertical)
+        }
+    }
+    
+    var userSideMenuOptions: some View {
+        VStack {
+            ForEach(SideMenuViewModel.allCases, id: \.rawValue) { viewModel in
+                if(viewModel == .profile) {
+                    NavigationLink {
+                        ProfileView()
+                    } label: {
+                        SideMenuOptionRowView(viewModel: viewModel)
+                    }
+                } else if (viewModel == .logout) {
+                    SideMenuOptionRowView(viewModel: viewModel)
+                } else {
+                    SideMenuOptionRowView(viewModel: viewModel)
+                }
+            }
+        }
     }
 }
