0d5f5f298e66dd5ecb5fb7a8ab7da7c91de04b70
Saurav Suman <sauravsuman001@hotmail.com>
2023-08-27 16:40:44 +0530

    connects the app to firebase console

--- TwitterClone_iOS/TwitterClone_iOSApp.swift
+++ TwitterClone_iOS/TwitterClone_iOSApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterClone_iOSApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView{
